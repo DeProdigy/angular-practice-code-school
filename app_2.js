ffbca8f70bde85d564b9190cfb255e1f67c529d1
DeProdigy <alex.hint.ah@gmail.com>
2014-05-16 04:44:23 -0400

    finish second level

--- app_2.js
+++ app_2.js
@@ -30,4 +30,16 @@
     }
   ];
 
+  app.controller('PanelController', function(){
+    this.tab = 1;
+
+    this.selectTab = function(setTab){
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+  });
+
 })();
